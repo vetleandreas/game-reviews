378e8b86dd8e11ad47f41baeebe9a38748f904af
Stefan Værnes <stefava@stud.ntnu.no>
2021-11-19 17:32:32 +0100

    Review Form Score: Ok

--- client/src/app-components.tsx
+++ client/src/app-components.tsx
@@ -446,19 +446,20 @@ export class GetGame extends Component {
                     className="me-sm-2"
                     id="inlineFormCustomSelect"
                     required
+                    value={this.formSelect}
                     onChange={(event) => (this.formSelect = event.currentTarget.value)}
                   >
                     <option value="0">Select rating</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
-                    <option value="3">4</option>
-                    <option value="3">5</option>
-                    <option value="3">6</option>
-                    <option value="3">7</option>
-                    <option value="3">8</option>
-                    <option value="3">9</option>
-                    <option value="3">10</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
                   </Form.Select>
                   <Form.Group className="mb-3" controlId="formReviewReviewText">
                     <Form.Label>Example textarea</Form.Label>
